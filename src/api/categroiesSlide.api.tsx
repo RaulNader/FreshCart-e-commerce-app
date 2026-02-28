d4398f83656b8359914b7161f03f7c0221718bb0
Raul Nader <raulnader23@gmail.com>
2026-03-01 00:19:06 +0200

    fix categories slide api build error

--- src/api/categroiesSlide.api.tsx
+++ src/api/categroiesSlide.api.tsx
@@ -1,5 +1,12 @@
-"use client";
+import type { Category } from "@/types/category.type";
 
 export async function getCategoriesSlide() {
+  const res = await fetch("https://ecommerce.routemisr.com/api/v1/categories", {
+    method: "GET",
+    headers: {},
+    next: { revalidate: 60 },
+  });
+
+  const { data } = (await res.json()) as { data: Category[] };
   return data;
 }
